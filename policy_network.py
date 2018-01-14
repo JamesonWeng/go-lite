9b9728d34b4418c1baf60871bd3eb4ee5a7d7f17
Jameson Weng <weng.jameson126@gmail.com>
2018-01-14 11:52:55 -0500

    defined output shape

--- policy_network.py
+++ policy_network.py
@@ -2,12 +2,14 @@ import logging
 import numpy as np
 import tensorflow as tf
 
+logging.basicConfig(level=logging.DEBUG)
+
 def create_conv_block(inputs, filters, layers):
 	current_tensor = inputs
 	for i in range(layers):
 		current_tensor = tf.layers.conv2d(
 			inputs=current_tensor,
-			filters=32,
+			filters=filters,
 			kernel_size=(3, 3),
 			padding='same',
 			activation=tf.nn.relu,
@@ -18,7 +20,8 @@ def create_conv_block(inputs, filters, layers):
 
 batch_size = 16
 num_rows, num_cols = (5, 10)
-input_size=(batch_size, num_rows, num_cols, 3)
+num_channels = 3
+input_size=(batch_size, num_rows, num_cols, num_channels)
 
 graph = tf.Graph()
 with graph.as_default():
@@ -29,13 +32,45 @@ with graph.as_default():
 	)
 
 	with tf.variable_scope('block1'):
-		output_tensor = create_conv_block(input_tensor, 32, 3)
+		block1_output = create_conv_block(input_tensor, 32, 3)
 
 	with tf.variable_scope('block2'):
-		output_tensor = create_conv_block(output_tensor, 64, 3)
+		block2_output = create_conv_block(block1_output, 64, 3)
 
 	with tf.variable_scope('block3'):
-		output_tensor = create_conv_block(output_tensor, 128, 3)
+		block3_output = create_conv_block(block2_output, 128, 3)
+
+	# the output is a probability distribution across the board
+	# with two channels: one for black's best move, one for white's best move
+	with tf.variable_scope('block4'):
+		conv_output = create_conv_block(block3_output, 2, 1)
+		logging.debug("conv_output shape: {}".format(conv_output.get_shape()))
+
+		# collapse height and width to perform softmax across both
+		reshape_output = tf.reshape(
+			conv_output,
+			shape=(batch_size, num_rows * num_cols, 2),
+			name='collapse_shape',
+		)
+		logging.debug("collapse_shape shape: {}".format(reshape_output.get_shape()))
+
+		# apply softmax
+		softmax_output = tf.nn.softmax(
+			reshape_output,
+			dim=1,
+			name='softmax',
+		)
+		logging.debug("softmax_output shape: {}".format(softmax_output.get_shape()))
+
+		# undo reshape
+		reshape_output = tf.reshape(
+			softmax_output,
+			shape=(batch_size, num_rows, num_cols, 2),
+			name='restore_shape',
+		)
+		logging.debug("restore_shape shape: {}".format(reshape_output.get_shape()))
+
+		output_tensor = reshape_output
 
 	init_op = tf.global_variables_initializer()
 	merged_summary_op = tf.summary.merge_all()
@@ -44,5 +79,6 @@ with graph.as_default():
 with tf.Session(graph=graph) as session:
 	session.run(init_op)
 	res = session.run(output_tensor, feed_dict={input_tensor: np.zeros(input_size)})
-	print(res)
+	# print(res)
+	# print(res.shape)
 	summary_writer = tf.summary.FileWriter('logs', graph=session.graph)
